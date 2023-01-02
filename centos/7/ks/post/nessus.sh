beb0d15d25e81577200820cef9002b967726de31
Chris Murray <cmurray@927.technology>
2023-01-02 16:44:53 -0600

    fixed curl command syntax and addd firewall rule to nessus.sh

--- centos/7/ks/post/nessus.sh
+++ centos/7/ks/post/nessus.sh
@@ -4,12 +4,13 @@ url=https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.4.1-es
 package=`echo ${url} | awk -F"/" '{print $NF}'`
 
 #download nessus rpm pakage per tennable preferred method
-curl --request GET                                                                                  \
-  --url '${url}'                                                                                    \
-  --output '${package}'
+curl --request GET --url ${url} --output ${package}
 
 #install the rpm
 rpm -ihv ${package}
 
 #enable the service
 systemctl enable nessusd
+
+#firewall
+firewall-offline-cmd --add-port=8834/tcp
