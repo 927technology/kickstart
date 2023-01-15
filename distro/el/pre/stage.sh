51813ea8ebaef58097a7de171c874d864f08606b
Chris Murray <cmurray@927.technology>
2023-01-14 21:57:16 -0600

    yah i was on crack

--- distro/el/pre/stage.sh
+++ distro/el/pre/stage.sh
@@ -4,6 +4,7 @@
 cmd_awk=/bin/awk
 cmd_cat=/bin/cat
 cmd_curl=/bin/curl
+cmd_echo=/bin/echo
 cmd_grep=/bin/grep
 cmd_head=/bin/head
 cmd_lsblk=/bin/lsblk
@@ -18,7 +19,7 @@ arch=`/bin/uname -p`
                                                                                                     #select the 1st block device only
 block_device=`${cmd_lsblk} | ${cmd_grep} disk[[:space:]]$ | ${cmd_head} -n 1 | ${cmd_awk} '{print $1}'`     
 block_device_size_raw=`${cmd_lsblk} | ${cmd_grep} disk[[:space:]]$ | ${cmd_awk} '{print $4}' | ${cmd_head} -n 1`
-url=https://raw.githubusercontent.com/927technology/kickstart/main
+url="https://raw.githubusercontent.com/927technology/kickstart/main"
 
 
 #main
@@ -36,9 +37,9 @@ fi
 block_device_size=`${cmd_echo} ${block_device_size_raw} | ${cmd_sed} 's/'${block_device_unit}'//g'`
 
 case ${VERSION_ID} in
-    7.*)    major_version=7     ;;
-    8.*)    major_version=8     ;;
-    9.*)    major_version=9     ;;
+    7 | 7.*)    major_version=7     ;;
+    8 | 8.*)    major_version=8     ;;
+    9 | 9.*)    major_version=9     ;;
 esac
 
 ${cmd_curl} ${url}/distro/${ID}/${major_version}/install/source/${arch}/cloud.ks 1> /tmp/cloud.ks 2>/dev/null
@@ -46,17 +47,17 @@ ${cmd_curl} ${url}/distro/${ID}/${major_version}/repo/${arch}/base.ks 1>  /tmp/r
 ${cmd_curl} ${url}/distro/${ID}/${major_version}/repo/${arch}/epel.ks 1>> /tmp/repo.ks 2>/dev/null
 
 
-${cmd_curl} ${url}/distro/el/${major_version}/partition/clear/${block_device}.ks 1> /tmp/partition.ks 2>/dev/null
+${cmd_curl} "${url}/distro/el/${major_version}/partition/clear/${block_device}.ks" 1> /tmp/partition.ks 2>/dev/null
 
 case ${block_device_unit} in
     g)
         if [ ${block_device_size} -ge 32 ]; then
-            ${cmd_curl} ${url}/distro/el/${major_version}/partition/32g.ks 1>> /tmp/partition.ks 2>/dev/null
+            ${cmd_curl} "${url}/distro/el/${major_version}/partition/32g.ks" 1>> /tmp/partition.ks 2>/dev/null
         else
-            ${cmd_curl} ${url}/distro/el/${major_version}/partition/auto.ks 1>> /tmp/partition.ks 2>/dev/null
+            ${cmd_curl} "${url}/distro/el/${major_version}/partition/auto.ks" 1>> /tmp/partition.ks 2>/dev/null
         fi
     ;;
     *) 
-        ${cmd_curl} ${url}/distro/el/${major_version}/partition/auto.ks 1>> /tmp/partition.ks 2>/dev/null
+        ${cmd_curl} "${url}/distro/el/${major_version}/partition/auto.ks" 1>> /tmp/partition.ks 2>/dev/null
     ;;
 esac
