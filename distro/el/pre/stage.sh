b542a22659c014b0c724600ce25386306beb09d8
Chris Murray <cmurray@927.technology>
2023-01-25 21:13:28 -0600

    adding addl recurse options to stage

--- distro/el/pre/stage.sh
+++ distro/el/pre/stage.sh
@@ -20,6 +20,13 @@ function config.get {
     ${cmd_echo} writing /tmp/${lsetting}.ks from ${ID} ${major_version}
     ${cmd_echo}
 
+    #Query ID/Arch
+    ${cmd_echo} ${ID}/${setting}/${arch}
+    ${cmd_curl} -sf ${url}/distro/${ID}/${lsetting}/${arch}/config.ks                                       >> /tmp/${lsetting}.ks
+    ${cmd_curl} -sf ${url}/distro/${ID}/${lsetting}/${arch}/${build}.ks                                     >> /tmp/${lsetting}.ks
+    ${cmd_echo} writing /tmp/${lsetting}.ks from ${ID} ${arch}
+    ${cmd_echo}
+
     #Query ID
     ${cmd_echo} ${ID}/${lsetting}
     ${cmd_curl} -sf ${url}/distro/${ID}/${lsetting}/config.ks                                       >> /tmp/${lsetting}.ks
@@ -41,10 +48,17 @@ function config.get {
     ${cmd_echo} writing /tmp/${lsetting}.ks from EL ${major_version}
     ${cmd_echo}
 
+    #Query EL/Arch
+    ${cmd_echo} el/${setting}/${arch}
+    ${cmd_curl} -sf ${url}/distro/el/${lsetting}/${arch}/${build}.ks                                          >> /tmp/${lsetting}.ks
+    ${cmd_curl} -sf ${url}/distro/el/${lsetting}/${arch}/config.ks                                          >> /tmp/${lsetting}.ks
+    ${cmd_echo} writing /tmp/${lsetting}.ks from EL ${arch}
+    ${cmd_echo}
+
     #Query EL
     ${cmd_echo} el/${lsetting}
-    ${cmd_curl} -sf ${url}/distro/el/${lsetting}/config.ks                                          >> /tmp/${lsetting}.ks
     ${cmd_curl} -sf ${url}/distro/el/${lsetting}/${build}.ks                                        >> /tmp/${lsetting}.ks
+    ${cmd_curl} -sf ${url}/distro/el/${lsetting}/config.ks                                          >> /tmp/${lsetting}.ks
     ${cmd_echo} writing /tmp/${lsetting}.ks from EL root
     ${cmd_echo}
 }
