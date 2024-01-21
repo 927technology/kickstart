fa8a68fa7e3da8544e14f5fcb4056b760815aa4a
Chris Murray <cmurray@927.technology>
2024-01-21 14:51:51 -0600

    added docker ks

---
+++ distro/el/post/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install yum utils
+yum install -y yum-utils
+
+#add docker repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+#remove other container packages
+yum remove -y docker docker-* podman runc
+
+#install docker-ce
+yum install -y docker-ce docker-ce-cli containerd.io
+
+# enable services
+systemctl enable docker
