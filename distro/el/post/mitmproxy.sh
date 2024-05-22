6cb106c1b68982c3e0aaf21842de110c0e4235c2
Chris Murray <cmurray@927.technology>
2024-05-22 18:55:39 -0500

    mitm #2

--- distro/el/post/mitmproxy.sh
+++ distro/el/post/mitmproxy.sh
@@ -1,3 +1,3 @@
-https://downloads.mitmproxy.org/10.3.0/mitmproxy-10.3.0-linux-x86_64.tar.gz
+wget https://downloads.mitmproxy.org/10.3.0/mitmproxy-10.3.0-linux-x86_64.tar.gz
 
 wget https://raw.githubusercontent.com/927technology/kickstart/main/files/mitmproxy-ca-cert.pem
