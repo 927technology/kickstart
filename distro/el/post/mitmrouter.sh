ff302a33110025754cc792caa7b014e0a382c87b
Chris Murray <cmurray@927.technology>
2024-05-27 20:11:41 -0500

    add mitmrouter ks update post script #3]4

--- distro/el/post/mitmrouter.sh
+++ distro/el/post/mitmrouter.sh
@@ -7,26 +7,26 @@ curl -sk https://raw.githubusercontent.com/927technology/kickstart/main/distro/e
 curl -sk https://raw.githubusercontent.com/927technology/kickstart/main/distro/el/post/header/mitmrouter.txt
 
 # install dependancies
-yum erase -y firewalld
-yum install -y wget tar iptables-utils iptables
+#yum erase -y firewalld
+#yum install -y wget tar iptables-utils iptables
 
 # create service account
 useradd -m -d /etc/mitm mitm
 
 # configure interface forwarding
-echo net.ipv4.ip_forward = 1              >> /etc/sysctl.d/mitmproxy.conf
-echo net.ipv6.conf.all.forwarding = 1     >> /etc/sysctl.d/mitmproxy.conf
-echo net.ipv4.conf.all.send_redirects = 0 >> /etc/sysctl.d/mitmproxy.conf
+echo net.ipv4.ip_forward = 1              >> /etc/sysctl.d/mitmrouter.conf
+echo net.ipv6.conf.all.forwarding = 1     >> /etc/sysctl.d/mitmrouter.conf
+echo net.ipv4.conf.all.send_redirects = 0 >> /etc/sysctl.d/mitmrouter.conf
 
 # configure firewall
-iptables  -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80  -j REDIRECT --to-port 8080
-iptables  -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j REDIRECT --to-port 8080
-ip6tables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80  -j REDIRECT --to-port 8080
-ip6tables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j REDIRECT --to-port 8080
+#iptables  -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80  -j REDIRECT --to-port 8080
+#iptables  -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j REDIRECT --to-port 8080
+#ip6tables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80  -j REDIRECT --to-port 8080
+#ip6tables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j REDIRECT --to-port 8080
 
 # create logging directory
-mkdir /var/log/mitmproxy
-chown mitm:mitm /var/log/mitmproxy
+mkdir /var/log/mitmrouter
+chown mitm:mitm /var/log/mitmrouter
 
 cat << EOF-MITMRouter > /sbin/mitmrouter.sh
 docker pull 927technology/mitmrouter:0.0.2
@@ -42,13 +42,13 @@ docker run                      \
   -e MODE=transparent           \
   -e WEB_HOST=0.0.0.0           \
   -v /etc/mitm:/etc/mitm        \
-  -v /var/log/mitmproxy:/var/log/mitmproxy  \
+  -v /var/log/mitmrouter:/var/log/mitmrouter  \
   927technology/mitmrouter:0.0.2
 
 EOF-MITMRouter
 
 chmod +x /sbin/mitmrouter.sh
 
-cat << EOF-Cron > /etc/cron.d/nessus
+cat << EOF-Cron > /etc/cron.d/mitmrouter
 @reboot mitm /sbin/mitmrouter.sh && rm -f /etc/cron.d/mitmrouter
 EOF-Cron
